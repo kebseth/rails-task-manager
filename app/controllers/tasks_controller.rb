b8fc79ab19fe51630d1b05b15116e16dfb0539cd
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-06 14:22:32 +0200

    checkbox dynamiques

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,56 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :edit, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # raise
+    @check = checked?
+  end
+  # UPDATE
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    # raise
+  end
+  # CREATE
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to task_path(task)
+  end
+
+  # DELETE
+  def destroy
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def checked?
+    if @task.completed
+      return 'This task is completed'
+    else
+      return 'This task is not completed'
+    end
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  resources :tasks
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # post 'tasks', to: 'tasks#create'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
